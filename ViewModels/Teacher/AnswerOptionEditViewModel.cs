893b371a7af87463026a6df40a76e8cadb5c21ef
BnnQ <1v25fa94@gmail.com>
2022-10-11 20:05:02 +0300

    Revert "AnswerOptionEditViewModel v0.1"
    This reverts commit 6b356162f7d706bed544adcdc1099d7da4a99e57.

--- ViewModels/Teacher/AnswerOptionEditViewModel.cs
+++ ViewModels/Teacher/AnswerOptionEditViewModel.cs
@@ -1,95 +1,13 @@
-﻿using HappyStudio.Mvvm.Input.Wpf;
-using MvvmBaseViewModels.Common;
-using NeoSmart.AsyncLock;
+﻿using MvvmBaseViewModels.Common;
 using TestingSystem.Models;
-using TestingSystem.Models.Contexts;
 
 namespace TestingSystem.ViewModels.Teacher
 {
     public class AnswerOptionEditViewModel : ViewModelBase
     {
-        private readonly AnswerOption answerOption;
-
-
-        private ushort serialNumberInQuestion;
-        public ushort SerialNumberInQuestion
+        public AnswerOptionEditViewModel(AnswerOption answerOption)
         {
-            get => serialNumberInQuestion;
-            set
-            {
-                if (serialNumberInQuestion != value)
-                {
-                    serialNumberInQuestion = value;
-                    OnPropertyChanged(nameof(SerialNumberInQuestion));
-                }
-            }
+
         }
-
-        private string content = null!;
-        public string Content
-        {
-            get => content;
-            set
-            {
-                if (content != value)
-                {
-                    content = value;
-                    OnPropertyChanged(nameof(Content));
-                }
-            }
-        }
-
-        private bool isCorrect;
-        public bool IsCorrect
-        {
-            get => isCorrect;
-            set
-            {
-                if (isCorrect != value)
-                {
-                    isCorrect = value;
-                    OnPropertyChanged(nameof(IsCorrect));
-                }
-            }
-        }
-
-        private readonly TestingSystemTeacherContext databaseContext;
-        private readonly AsyncLock databaseContextLocker;
-
-
-        public AnswerOptionEditViewModel(TestingSystemTeacherContext databaseContext, AsyncLock databaseContextLocker, AnswerOption answerOption)
-        {
-            this.answerOption = answerOption;
-            this.databaseContext = databaseContext;
-            this.databaseContextLocker = databaseContextLocker;
-
-            SerialNumberInQuestion = answerOption.SerialNumberInQuestion;
-            Content = answerOption.Content;
-            IsCorrect = answerOption.IsCorrect;
-        }
-
-
-        #region Commands
-        private RelayCommand okCommand = null!;
-        public RelayCommand OkCommand
-        {
-            get => okCommand ??= new(() =>
-            {
-                answerOption.SerialNumberInQuestion = SerialNumberInQuestion;
-                answerOption.Content = Content;
-                answerOption.IsCorrect = IsCorrect;
-
-                Close(true);
-            }, () => SerialNumberInQuestion > 0 && !string.IsNullOrWhiteSpace(Content));
-        }
-
-        private RelayCommand cancelCommand = null!;
-        public RelayCommand CancelCommand
-        {
-            get => cancelCommand ??= new(() => Close(false));
-        }
-        #endregion
-
-
     }
 }
