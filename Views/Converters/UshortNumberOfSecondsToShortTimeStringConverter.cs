60c5b553341250dd09cd9e7277ce90651ad3bfcd
BnnQ <1v25fa94@gmail.com>
2022-10-16 02:46:15 +0300

    Converter UshortNumberOfSecondsToShortTime added

---
+++ Views/Converters/UshortNumberOfSecondsToShortTimeStringConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestingSystem.Views.Converters
+{
+    public class UshortNumberOfSecondsToShortTimeStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ushort numberOfSeconds)
+            {
+                return new DateTime(TimeSpan.TicksPerSecond * numberOfSeconds).ToShortTimeString();
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string shortTimeString && !string.IsNullOrWhiteSpace(shortTimeString))
+            {
+                string[] splittedShortTimeString = shortTimeString.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                byte hours = byte.Parse(splittedShortTimeString.First());
+                byte minutes = byte.Parse(splittedShortTimeString.Last());
+
+                const ushort SecondsInMinute = 60;
+                const ushort MinutesInHour = 60;
+                const ushort SecondsInHour = 1 * SecondsInMinute * MinutesInHour;
+
+                ushort numberOfSeconds = (ushort) ((hours * SecondsInHour) + (minutes * SecondsInMinute));
+                return numberOfSeconds;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+    }
+}
