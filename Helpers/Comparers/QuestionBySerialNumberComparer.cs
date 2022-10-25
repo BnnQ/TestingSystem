229d9ddbc745e87ba3351fdb434354d10b895a40
BnnQ <1v25fa94@gmail.com>
2022-10-25 09:48:46 +0300

    Added helper QuestionBySerialNumberComparer

---
+++ Helpers/Comparers/QuestionBySerialNumberComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TestingSystem.Models;
+
+namespace TestingSystem.Helpers.Comparers
+{
+    public class QuestionBySerialNumberComparer : IComparer<Question>
+    {
+        public int Compare(Question? x, Question? y)
+        {
+            if (x is null || y is null)
+                return 0;
+            else
+                return x.SerialNumberInTest - y.SerialNumberInTest;
+        }
+    }
+}
