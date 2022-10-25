62fa27b6818f7dfddbdf22824c018a5e0759623c
BnnQ <1v25fa94@gmail.com>
2022-10-25 09:48:58 +0300

    Added helper TestCompletedNavigationArgs

---
+++ Helpers/CustomNavigationArgs/TestCompletedNavigationArgs.cs
@@ -0,0 +1,17 @@
+﻿using MvvmBaseViewModels.Common.Interfaces;
+using MvvmBaseViewModels.Navigation;
+using TestingSystem.Models;
+
+namespace TestingSystem.Helpers.CustomNavigationArgs
+{
+    public class TestCompletedNavigationArgs : NavigationArgs
+    {
+        public TestResults TestResults { get; init; }
+
+        public TestCompletedNavigationArgs(ICloseable? viewModelNavigatedFrom, TestResults testResults) : base(viewModelNavigatedFrom)
+        {
+            TestResults = testResults;
+        }
+
+    }
+}
