3a0cc3590b40d59353c39f4adde9bfdcd62721c9
BnnQ <1v25fa94@gmail.com>
2022-10-25 09:46:43 +0300

    TestInfoView v1.0

---
+++ Views/Student/TestInfoView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MvvmBaseViewModels.Helpers;
+using System.Windows;
+using TestingSystem.Models;
+using TestingSystem.ViewModels.Student;
+
+namespace TestingSystem.Views.Student
+{
+    /// <summary>
+    /// Interaction logic for TestInfoView.xaml
+    /// </summary>
+    public partial class TestInfoView : Window
+    {
+        private readonly TestInfoViewModel viewModel;
+        public TestInfoView(Test test, Models.Student student)
+        {
+            InitializeComponent();
+
+            viewModel = new TestInfoViewModel(test, student);
+            viewModel.Closed += (_) => Application.Current?.Dispatcher.Invoke(Close);
+            viewModel.ErrorMessageOccurred += DefaultMessageHandlers.HandleError;
+            viewModel.ErrorMessageOccurred += (_) => Application.Current?.Dispatcher.Invoke(Close);
+            viewModel.CriticalErrorMessageOccured += (exception) =>
+                DefaultMessageHandlers.HandleCriticalError(this, exception);
+
+            DataContext = viewModel;
+            Dispatcher.ShutdownStarted += (_, _) =>
+            {
+                if (viewModel?.IsClosed == false)
+                    viewModel.Close();
+            };
+        }
+    }
+}
