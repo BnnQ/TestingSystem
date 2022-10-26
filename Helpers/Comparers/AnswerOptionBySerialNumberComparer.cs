d53c18fc0d4c6d5c989730ece14db72f7c486f52
BnnQ <1v25fa94@gmail.com>
2022-10-26 07:43:01 +0300

    Added AnswerOptionBySerialNumberComparer

---
+++ Helpers/Comparers/AnswerOptionBySerialNumberComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TestingSystem.Models;
+
+namespace TestingSystem.Helpers.Comparers
+{
+    public class AnswerOptionBySerialNumberComparer : IComparer<AnswerOption>
+    {
+        public int Compare(AnswerOption? x, AnswerOption? y)
+        {
+            if (x is null || y is null)
+                return 0;
+
+            return x.SerialNumberInQuestion - y.SerialNumberInQuestion;
+        }
+    }
+}
