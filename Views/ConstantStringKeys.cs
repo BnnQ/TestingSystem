16bda6d590697beeb01a0dd62590b3339f6b3242
BnnQ <1v25fa94@gmail.com>
2022-10-26 22:21:56 +0300

    Added global converters: objectToBoolByHasValueConverter; objectToBoolByHasNotValueConverter; stringIsLoadedToBoolIsBusyConverter; Added ConstantStringKeys

---
+++ Views/ConstantStringKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace TestingSystem.Views
+{
+    public static class ConstantStringKeys
+    {
+        public const string NotLoadedState = "NotLoaded";
+        public const string LoadedState = "Loaded";
+    }
+}
