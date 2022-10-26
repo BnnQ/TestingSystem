6f5ada0daef0e8a611fe177ea1c480aaea48cbb2
BnnQ <1v25fa94@gmail.com>
2022-10-26 07:46:53 +0300

    TestResultsViewModel v1.0

--- ViewModels/Student/TestResultsViewModel.cs
+++ ViewModels/Student/TestResultsViewModel.cs
@@ -1,15 +1,48 @@
 ﻿using Egor92.MvvmNavigation.Abstractions;
+using HappyStudio.Mvvm.Input.Wpf;
 using MvvmBaseViewModels.Navigation;
+using TestingSystem.Helpers.CustomNavigationArgs;
 using TestingSystem.Models;
 
 namespace TestingSystem.ViewModels.Student
 {
     public class TestResultsViewModel : NavigationViewModelBase
     {
-        public TestResultsViewModel(INavigationManager navigationManager, TestResults? testResults) : base(navigationManager)
+        private TestResults? testResults;
+        public TestResults? TestResults
         {
-
+            get => testResults;
+            set
+            {
+                if (testResults != value)
+                {
+                    testResults = value;
+                    OnPropertyChanged(nameof(TestResults));
+                }
+            }
         }
+
+
+        public TestResultsViewModel(INavigationManager navigationManager) : base(navigationManager)
+        {
+            //empty
+        }
+
+
+        public override void OnNavigatedTo(object arg)
+        {
+            base.OnNavigatedTo(arg);
+            if (arg is TestCompletedNavigationArgs testCompletedArgs)
+                TestResults = testCompletedArgs.TestResults;
+        }
+
+        #region Commands
+        private RelayCommand okCommand = null!;
+        public RelayCommand OkCommand
+        {
+            get => okCommand ??= new(() => Close());
+        }
+        #endregion
 
     }
 }
