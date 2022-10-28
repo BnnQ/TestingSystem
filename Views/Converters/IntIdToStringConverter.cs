c99d552bc85dd109bbb67a53ee5b959e3fb30420
BnnQ <1v25fa94@gmail.com>
2022-10-28 07:15:35 +0300

    TeacherView v1.0 (added unique id in the corner of the test card

---
+++ Views/Converters/IntIdToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TestingSystem.Views.Converters
+{
+    public class IntIdToStringConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int id)
+                return $"{id:D5}";
+            else
+                return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringId && int.TryParse(stringId, out int id))
+                return id;
+            else
+                return null;
+        }
+    }
+}
