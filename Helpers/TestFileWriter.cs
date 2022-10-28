eedd312834e754edcc78178fa7598f8f40130841
BnnQ <1v25fa94@gmail.com>
2022-10-28 04:04:56 +0300

    Added TestFileWriter

---
+++ Helpers/TestFileWriter.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Models;
+
+namespace TestingSystem.Helpers
+{
+    public static class TestFileWriter
+    {
+        public static async Task WriteToTextFileAsync(Test test, string path, bool includeCorrectAnswers = false)
+        {
+            string testString = null!;
+            await Task.Run(() =>
+            {
+                StringBuilder testStringBuilder = new StringBuilder()
+                                                  .AppendLine(test.Name)
+                                                  .AppendLine();
+
+                foreach (Question question in test.Questions)
+                {
+                    testStringBuilder.AppendLine($"{question.SerialNumberInTest}) {question.Content}");
+                    foreach (AnswerOption answerOption in question.AnswerOptions)
+                        testStringBuilder.AppendLine($"{(includeCorrectAnswers && answerOption.IsCorrect ? '✓' : ' ')}{answerOption.SerialNumberInQuestion}. {answerOption.Content}");
+
+                    testStringBuilder
+                        .AppendLine()
+                        .AppendLine();
+                }
+
+                testString = testStringBuilder.ToString();
+            });
+
+            await File.WriteAllTextAsync(path, testString);
+        }
+    }
+}
