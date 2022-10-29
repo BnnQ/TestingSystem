a30acf1852ffa8ad841479ea49d4356ae7c40871
BnnQ <1v25fa94@gmail.com>
2022-10-29 07:02:19 +0300

    [Teacher] MainContainer v1.0

---
+++ ViewModels/Teacher/MainContainerViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Egor92.MvvmNavigation.Abstractions;
+using HappyStudio.Mvvm.Input.Wpf;
+using MvvmBaseViewModels.Common;
+using MvvmBaseViewModels.Navigation;
+using System.Windows;
+using TestingSystem.Constants.Teacher;
+using TestingSystem.Views.Authorization;
+
+namespace TestingSystem.ViewModels.Teacher
+{
+    public class MainContainerViewModel : ViewModelBase
+    {
+        private readonly INavigationManager navigationManager;
+        public Models.Teacher Teacher { get; init; }
+
+
+        public MainContainerViewModel(INavigationManager navigationManager, Models.Teacher teacher)
+        {
+            this.navigationManager = navigationManager;
+            Teacher = teacher;
+        }
+
+
+        #region Commands
+
+        #region Navigation
+        private string lastNavigationKey = NavigationKeys.Main;
+
+        private RelayCommand switchToMainNavigationCommand = null!;
+        public RelayCommand SwitchToMainNavigationCommand
+        {
+            get => switchToMainNavigationCommand ??= new(() =>
+            {
+                navigationManager.Navigate(lastNavigationKey = NavigationKeys.Main,
+                    new NavigationArgs(this));
+            }, () => navigationManager.CanNavigate(NavigationKeys.Main) && lastNavigationKey != NavigationKeys.Main);
+        }
+
+        private RelayCommand switchToStatisticsNavigationCommand = null!;
+        public RelayCommand SwitchToStatisticsNavigationCommand
+        {
+            get => switchToStatisticsNavigationCommand ??= new(() =>
+            {
+                navigationManager.Navigate(lastNavigationKey = NavigationKeys.Statistics, 
+                    new NavigationArgs(this));
+            }, () => navigationManager.CanNavigate(NavigationKeys.Statistics) && lastNavigationKey != NavigationKeys.Statistics);
+        }
+
+        private RelayCommand switchToAboutNavigationCommand = null!;
+        public RelayCommand SwitchToAboutNavigationCommand
+        {
+            get => switchToAboutNavigationCommand ??= new(() =>
+            {
+                navigationManager.Navigate(lastNavigationKey = NavigationKeys.About, 
+                    new NavigationArgs(this));
+            }, () => navigationManager.CanNavigate(NavigationKeys.About) && lastNavigationKey != NavigationKeys.About);
+        }
+        #endregion
+
+        #region PopupBox
+        private RelayCommand exitCommand = null!;
+        public RelayCommand ExitCommand
+        {
+            get => exitCommand ??= new(() => Application.Current?.Shutdown());
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ Views/Teacher/MainContainerView.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Egor92.MvvmNavigation;
+using MvvmBaseViewModels.Helpers;
+using MvvmBaseViewModels.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using TestingSystem.Constants.Teacher;
+using TestingSystem.ViewModels;
+using TestingSystem.ViewModels.Teacher;
+
+namespace TestingSystem.Views.Teacher
+{
+    /// <summary>
+    /// Interaction logic for MainContainerView.xaml
+    /// </summary>
+    public partial class MainContainerView : Window
+    {
+        private readonly MainContainerViewModel containerViewModel;
+        private readonly MainViewModel mainViewModel;
+        private readonly StatisticsViewModel statisticsViewModel;
+        private readonly AboutViewModel aboutViewModel;
+        private readonly NavigationManager navigationManager;
+
+        public MainContainerView(Models.Teacher teacher)
+        {
+            InitializeComponent();
+            Application.Current?.Dispatcher.Invoke(() =>
+            {
+                IsEnabled = false;
+                Mouse.OverrideCursor = Cursors.Wait;
+            });
+
+            navigationManager = new(FrameContent);
+
+            containerViewModel = new(navigationManager, teacher);
+            containerViewModel.Closed += (_) => Application.Current?.Dispatcher.Invoke(Close);
+
+            mainViewModel = new(navigationManager, teacher);
+            mainViewModel.Closed += (_) =>
+            {
+                if (containerViewModel?.IsClosed == false)
+                    containerViewModel.Close();
+            };
+            mainViewModel.ErrorMessageOccurred += (exception) => DefaultMessageHandlers.HandleError(this, exception);
+            mainViewModel.CriticalErrorMessageOccured += (exception) => 
+                DefaultMessageHandlers.HandleCriticalError(this, exception);
+
+            statisticsViewModel = new(navigationManager);
+            statisticsViewModel.Closed += (_) =>
+            {
+                if (containerViewModel?.IsClosed == false)
+                    containerViewModel.Close();
+            };
+            statisticsViewModel.CriticalErrorMessageOccured += (exception) =>
+                DefaultMessageHandlers.HandleCriticalError(this, exception);
+
+            aboutViewModel = new();
+            aboutViewModel.Closed += (_) =>
+            {
+                if (containerViewModel?.IsClosed == false)
+                    containerViewModel.Close();
+            };
+
+            mainViewModel.InitialLoaderBackgroundWorker.WorkCompleted += () =>
+            {
+                ConfigureNavigation();
+                DataContext = containerViewModel;
+                IsEnabled = true;
+                Mouse.OverrideCursor = Cursors.Arrow;
+            };
+            Dispatcher.ShutdownStarted += (_, _) =>
+            {
+                if (mainViewModel?.IsClosed == false)
+                    mainViewModel?.Close();
+
+                if (statisticsViewModel?.IsClosed == false)
+                    statisticsViewModel?.Close();
+
+                if (aboutViewModel?.IsClosed == false)
+                    aboutViewModel?.Close();
+
+                if (containerViewModel?.IsClosed == false)
+                    containerViewModel?.Close();
+            };
+        }
+
+        private void ConfigureNavigation()
+        {
+            navigationManager.Register<MainView>(NavigationKeys.Main, mainViewModel);
+            navigationManager.Register<StatisticsView>(NavigationKeys.Statistics, statisticsViewModel);
+            navigationManager.Register<AboutView>(NavigationKeys.About, aboutViewModel);
+            
+            navigationManager.Navigate(NavigationKeys.Main, new NavigationArgs(containerViewModel));
+        }
+
+    }
+}
