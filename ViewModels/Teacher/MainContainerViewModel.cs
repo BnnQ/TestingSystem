ca0153828e2b6d42d9d73dced26252006141e438
BnnQ <1v25fa94@gmail.com>
2022-10-30 00:36:49 +0300

    [Teacher] MainContainer v1.1 (Left navigation drawer now closes when switching to another view)

--- ViewModels/Teacher/MainContainerViewModel.cs
+++ ViewModels/Teacher/MainContainerViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using Egor92.MvvmNavigation.Abstractions;
 using HappyStudio.Mvvm.Input.Wpf;
+using MaterialDesignThemes.Wpf;
 using MvvmBaseViewModels.Common;
 using MvvmBaseViewModels.Navigation;
 using System.Windows;
+using System.Windows.Controls;
 using TestingSystem.Constants.Teacher;
-using TestingSystem.Views.Authorization;
 
 namespace TestingSystem.ViewModels.Teacher
 {
@@ -33,6 +34,7 @@ namespace TestingSystem.ViewModels.Teacher
             {
                 navigationManager.Navigate(lastNavigationKey = NavigationKeys.Main,
                     new NavigationArgs(this));
+                DrawerHost.CloseDrawerCommand.Execute(Dock.Left, null);
             }, () => navigationManager.CanNavigate(NavigationKeys.Main) && lastNavigationKey != NavigationKeys.Main);
         }
 
@@ -43,6 +45,7 @@ namespace TestingSystem.ViewModels.Teacher
             {
                 navigationManager.Navigate(lastNavigationKey = NavigationKeys.Statistics, 
                     new NavigationArgs(this));
+                DrawerHost.CloseDrawerCommand.Execute(Dock.Left, null);
             }, () => navigationManager.CanNavigate(NavigationKeys.Statistics) && lastNavigationKey != NavigationKeys.Statistics);
         }
 
@@ -53,6 +56,7 @@ namespace TestingSystem.ViewModels.Teacher
             {
                 navigationManager.Navigate(lastNavigationKey = NavigationKeys.About, 
                     new NavigationArgs(this));
+                DrawerHost.CloseDrawerCommand.Execute(Dock.Left, null);
             }, () => navigationManager.CanNavigate(NavigationKeys.About) && lastNavigationKey != NavigationKeys.About);
         }
         #endregion
