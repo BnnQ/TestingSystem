80e079fbee6b137b42593c9e5e061993d56de783
BnnQ <1v25fa94@gmail.com>
2022-10-31 09:31:03 +0200

    Added DateTimeToStringConverter

---
+++ Views/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TestingSystem.Views.Converters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+                return dateTime.ToString(CultureInfo.CurrentCulture);
+
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string dateTimeString && DateTime.TryParse(dateTimeString, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime dateTime))
+                return dateTime;
+
+            return null;
+        }
+    }
+}
