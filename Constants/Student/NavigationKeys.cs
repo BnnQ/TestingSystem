cb05c1996d9beb536aadfaa6b91621661e9bc33e
BnnQ <1v25fa94@gmail.com>
2022-10-31 09:24:47 +0200

    [Student] Added NavigationKeys constants

--- Constants/Student/NavigationKeys.cs
+++ Constants/Student/NavigationKeys.cs
@@ -2,7 +2,11 @@
 {
     public static class NavigationKeys
     {
-        public const string TestPassing = "TestPassing";
-        public const string TestResults = "TestResults";
+        public const string Main = nameof(Main);
+        public const string Statistics = nameof(Statistics);
+        public const string About = nameof(About);
+
+        public const string TestPassing = nameof(TestPassing);
+        public const string TestResults = nameof(TestResults);
     }
 }
