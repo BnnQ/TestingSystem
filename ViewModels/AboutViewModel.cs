6c3178126b2a17b791b31121df5adc3187a982c8
BnnQ <1v25fa94@gmail.com>
2022-10-31 18:15:28 +0200

    AboutViewModel v1.0

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,9 +1,38 @@
-﻿using MvvmBaseViewModels.Common;
+﻿using HappyStudio.Mvvm.Input.Wpf;
+using MvvmBaseViewModels.Common;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
 
 namespace TestingSystem.ViewModels
 {
     public class AboutViewModel : ViewModelBase
     {
+        public Uri GithubUrl { get; init; }
+        public Uri IconUrl { get; init; }
+        public Uri DesignLibraryUrl { get; init; }
 
+        public AboutViewModel()
+        {
+            GithubUrl = new Uri("https://github.com/BnnQ/TestingSystem", UriKind.Absolute);
+            IconUrl = new Uri("https://icons8.com/icon/GGgmJKaq5zTO/test", UriKind.Absolute);
+            DesignLibraryUrl = new Uri("https://github.com/ButchersBoy/MaterialDesignInXamlToolkit", UriKind.Absolute);
+        }
+
+        #region Commands
+        private RelayCommand<Uri> openUrlInBrowserCommand = null!;
+        public RelayCommand<Uri> OpenUrlInBrowserCommand
+        {
+            get => openUrlInBrowserCommand ??= new((url) =>
+            {
+                ProcessStartInfo processStartInfo = new();
+                processStartInfo.FileName = url!.AbsoluteUri;
+                processStartInfo.UseShellExecute = true;
+
+                Process.Start(processStartInfo);
+            }, (url) => url is not null && !string.IsNullOrWhiteSpace(url.AbsoluteUri));
+        }
+        #endregion
     }
 }
