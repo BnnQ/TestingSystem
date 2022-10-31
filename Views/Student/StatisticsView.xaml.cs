a23fade5b6225d7dc159e71a739b70abc315ce2f
BnnQ <1v25fa94@gmail.com>
2022-10-31 09:08:18 +0200

    [Student] StatisticsView v1.0

---
+++ Views/Student/StatisticsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TestingSystem.Views.Student
+{
+    /// <summary>
+    /// Interaction logic for StatisticsView.xaml
+    /// </summary>
+    public partial class StatisticsView : UserControl
+    {
+        public StatisticsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
