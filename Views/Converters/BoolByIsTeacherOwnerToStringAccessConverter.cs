d0aaeeaf3889a2af5756b9d46d2f327b3299f021
BnnQ <1v25fa94@gmail.com>
2022-10-31 08:24:03 +0200

    [Teacher] Added constant AccessModes

---
+++ Views/Converters/BoolByIsTeacherOwnerToStringAccessConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using TestingSystem.Constants.Teacher;
+
+namespace TestingSystem.Views.Converters
+{
+    public class BoolByIsTeacherOwnerToStringAccessConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isTeacherOwner)
+                return isTeacherOwner ? AccessModes.ReadAndWrite : AccessModes.ReadOnly;
+
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string accessMode)
+                return accessMode.Equals(AccessModes.ReadAndWrite) ? true : accessMode.Equals(AccessModes.ReadOnly) ? AccessModes.ReadOnly : null;
+
+            return null;
+        }
+
+    }
+}
