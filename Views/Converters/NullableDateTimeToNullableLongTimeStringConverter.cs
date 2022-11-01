4903004cee07d6f03a729517b3046127497b3e66
BnnQ <1v25fa94@gmail.com>
2022-11-01 11:28:21 +0200

    Added NullableDateTimeToNullableLongTimeStringConverter

---
+++ Views/Converters/NullableDateTimeToNullableLongTimeStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TestingSystem.Views.Converters
+{
+    public class NullableDateTimeToNullableLongTimeStringConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime? dateTime = value as DateTime?;
+            if (dateTime.HasValue)
+                return dateTime.Value.ToLongTimeString();
+
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string longTimeString && !string.IsNullOrWhiteSpace(longTimeString))
+                return DateTime.Parse(longTimeString);
+
+            return null;
+        }
+    }
+}
